7b79db95e4db40d683a6762aaee03a8eacd0a818
rschavan1709 <rschavan1709@gmail.com>
2023-01-12 18:48:08 +0530

    changes

--- UserService/UserService/src/main/java/com/neosoft/user/service/UserServiceApplication.java
+++ UserService/UserService/src/main/java/com/neosoft/user/service/UserServiceApplication.java
@@ -11,5 +11,5 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
+	System.out.println("microservice");
 }
